79fdf17a044a5c9f9fc5a8c43d569f897a606c37
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-07 16:35:22 +0530

    04 Control Flow

---
+++ 04_control_flow_JS/switch.js
@@ -0,0 +1,39 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+
+    default:
+        console.log("Ye month mere pass nhi h");
+        break;
+}
