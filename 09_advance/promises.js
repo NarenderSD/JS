5e9f324f32d5f50cbdaaa62389cc5a2f1b71a740
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-13 17:30:20 +0530

    promise1

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -3,8 +3,61 @@ const promiseOne = new Promise(function(resolve, reject){
     // DB calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is compelete');
+        resolve()
 
 
     }, 1000)
 
 })
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async 2");
+        resolve
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Narender", email: "narendersdcs@gmail.com"})
+
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+
+})
+
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if (!error) {
+            resolve({username: "Narender", password: "859596"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+
+    }, 1000)
+})
+
+promiseFour.then((user) => {
+
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+// console.log(username);
