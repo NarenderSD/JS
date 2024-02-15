8c94773d95501c492be7a2a1bb556cf499f42f9f
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-15 07:27:30 +0530

    V50 test arry

---
+++ 12_Array/test array.js
@@ -0,0 +1,12 @@
+const colors = ["red", "yellow", "blue"];
+colors[5] = "purple";
+colors.forEach((item, index) => {
+  console.log(`${index}: ${item}`);
+});
+// Output:
+// 0: red
+// 1: yellow
+// 2: blue
+// 5: purple
+
+colors.reverse(); // ['purple', empty × 2, 'blue', 'yellow', 'red']
